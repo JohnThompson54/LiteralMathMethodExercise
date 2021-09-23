ef67f3f68818615db70d654d39d2874a876d17b2
Your Name <you@example.com>
2021-09-23 08:35:39 -0500

    Literal Math Method Exercise in intro to Javascript

---
+++ script.js
@@ -0,0 +1,58 @@
+
+// 1a
+let warmHugs = "Hi, I'm Olaf and I like warm hugs."
+
+// 1b
+console.log("warmHugs".toUpperCase());
+
+console.log(warmHugs.toUpperCase());
+
+// 1c
+console.log(warmHugs.replace("like", "love"));
+
+// 2a
+let beenImpaled = "Oh, look at that. I've been impaled."
+console.log(beenImpaled);
+
+// 2b
+console.log(beenImpaled.slice(18, 37));
+
+// 3a
+let dotDotDot = "..."
+console.log(dotDotDot);
+
+// 3b
+let skullBones = (`I don't have a skull` + dotDotDot + `or bones`)
+console.log(skullBones);
+
+// 4
+console.log(Math.PI);
+
+// 5
+
+let randomNumber = Math.random();
+console.log(randomNumber);
+randomNumber *= 3;
+console.log(randomNumber);
+randomNumber = Math.floor(randomNumber);
+randomNumber++;
+console.log(randomNumber);
+
+// Bonus
+
+// 6
+let LetGo = " Let it Go!"
+console.log(LetGo.repeat(2));
+
+// 7a
+let reindeers = "Reindeers are better than people."
+console.log(reindeers.replace(" ", "_",).replace(" ", "_").replace(" ", "_").replace(" ", "_"));
+
+// 8
+console.log(Math.sqrt(2));
+
+// 9
+//let newRandomNumber = (Math.random(), newRandomNumber *=16, newRandomNumber = (Math.floor(newRandomNumber+7)))
+//console.log(newRandomNumber);
+
+
